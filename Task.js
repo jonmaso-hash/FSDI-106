01493416e00a41b3c83ee74e000b3be33fa5ac8d
jonmaso-hash <jonmaso@gmail.com>
2025-12-05 09:46:27 -0800

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,12 @@
+class Task{
+    constructor(title, color, date, status, description, budget){
+        this.title = title;
+        this.color = color;
+         this.date = date;
+          this.status = status;
+           this.description = description;
+        this.budget = budget;
+    }
+    
+    
+}
